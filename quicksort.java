773d35fa51b90423bce41e22ee0c00df7f54c77a
Luke <burde041@morris.umn.edu>
2022-09-04 18:35:28 -0500

    Fix quicksort to work and add test for proper sorting with comment on amount of comparisons.

--- quicksort.java
+++ quicksort.java
@@ -3,28 +3,34 @@ import java.util.Arrays;
 class quicksort{
     public static void main(String[] args) {
         //Sets length of testing array
-        int testLength = 5;
+        int testLength = 10;
         TestInteger[] testIntegersQ = new TestInteger[testLength];
         for(int x=0;x<testLength;x++)
-            testIntegersQ[x] = new TestInteger((int)Math.random());
+            testIntegersQ[x] = new TestInteger((int)(Math.random() * 1000000));
         TestInteger[] testIntegersM = testIntegersQ.clone();
         Arrays.sort(testIntegersM);
-        qsort(testIntegersQ, 0, testIntegersQ.length-1);
+        quicksort q = new quicksort();
+        q.qsort(testIntegersQ, 0, testIntegersQ.length-1);
+        if(Arrays.equals(testIntegersM, testIntegersQ)) {
+            System.out.println("The array was properly sorted. The amount of comparisons was: " + testIntegersQ[0].getCounter());
+        }
+        else
+            System.out.println("The array was not properly sorted.");
     }
 
-    public static void qsort(TestInteger[] A, int low, int high){
-        if(low>=high || low<0)
-            return;
-        int p = partition(A, low, high);
-        qsort(A, low, p-1);
-        qsort(A, p+1, high);
+    void qsort(TestInteger[] A, int low, int high){
+        if(low<high){
+            int p = partition(A, low, high);
+            qsort(A, low, p-1);
+            qsort(A, p+1, high);
+        }
     }
 
-    public static int partition(TestInteger[] A, int low, int high){
+    int partition(TestInteger[] A, int low, int high){
         TestInteger temp;
         TestInteger pivot = A[high];
         int i = low-1;
-        for(int j = low; j < high-1; j++){
+        for(int j = low; j < high; j++){
             if(A[j].compareTo(pivot) != 1){
                 i++;
                 temp = A[i];
@@ -32,11 +38,10 @@ class quicksort{
                 A[j] = temp;
             }
         }
-        i++;
-        temp = A[i];
-        A[i] = A[high];
+        temp = A[i+1];
+        A[i+1] = A[high];
         A[high] = temp;
-        return i;
+        return i+1;
     }
 }
 
@@ -44,7 +49,7 @@ class TestInteger implements Comparable<TestInteger>{
     public TestInteger(int v){
         this.value = v;
     }
-    private int value;
+    private final int value;
     public static long counter;
     public int compareTo(TestInteger other){
         ++counter;
